2b10578672e79a5af8d29a22f14515bd64ec46a3
tan-de-xiang <94498708+tan-de-xiang@users.noreply.github.com>
2021-11-17 08:54:44 +0800

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,25 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(20,100,18,33)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x + 2
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x - 2
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y - 2
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y + 2
+  }
+drawSprites();
+}
+
